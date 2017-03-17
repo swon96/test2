85e718420057d6037ee21bb7020782eacbe95183
seung96 <oh.seungwon@ssarc.re.kr>
2017-03-17 18:17:50 +0900

    테스트용 폴더구조 생성

---
+++ src/main/java/se/smu/todo_project_sevenbullet/src/javaapplication2/todo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Song
+ */
+class todo {
+	String t_name;
+	String t_task;
+
+	public todo(String todo_name, String todo_task) {
+		t_name = todo_name;
+		t_task = todo_task;
+	}
+
+}
